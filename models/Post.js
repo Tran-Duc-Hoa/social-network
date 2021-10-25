724a86bb6890a16497e2505bfe027575337735c6
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2021-10-25 21:37:35 +0700

    Create Post model

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PostSchema = new Schema(
+    {
+        user: {
+            type: ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        avatar: String,
+        likes: [
+            {
+                user: {
+                    type: ObjectId,
+                    ref: 'User',
+                    required: true,
+                },
+            },
+        ],
+        comments: [
+            {
+                user: {
+                    type: ObjectId,
+                    ref: 'User',
+                    required: true,
+                },
+                text: {
+                    type: String,
+                    required: true,
+                },
+                avatar: String,
+            },
+        ],
+    },
+    {
+        collection: 'Post',
+        timestamps: true,
+    }
+);
+
+module.exports = Post = new mongoose.model('Post', PostSchema);
