6cf19a7976ac587deaa7b5810261f30cfd2f0b21
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2021-11-23 21:54:25 +0700

    Add Login, Register form

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profiles', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = 3001;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
