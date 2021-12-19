9cdf6f7518ab076c6a57630ac15d95dfc8a7cb3d
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2021-12-19 22:13:02 +0700

    Add CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-form/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
@@ -34,6 +35,7 @@ const App = () => {
                         <Route exact path="/register" component={Register} />
                         <Route exact path="/login" component={Login} />
                         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                        <Route exact path="/create-profile" component={CreateProfile} />
                     </Switch>
                 </section>
             </Router>
