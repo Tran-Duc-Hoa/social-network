7d3c91e23c66bd490f170f4b49d2b741f7c6ff75
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2022-01-17 21:07:05 +0700

    Finish Profile Actions & Producer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,6 +6,8 @@ import {
     UPDATE_PROFILE,
     CLEAR_PROFILE,
     ACCOUNT_DELETED,
+    GET_PROFILES,
+    GET_REPOS,
 } from './types';
 
 // Get current user profile
@@ -15,6 +17,73 @@ export const getCurrentProfile = () => async (dispatch) => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Get profile success', 'success'));
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status },
+        });
+
+        dispatch(setAlert('Get profile fail', 'danger'));
+    }
+};
+
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Get profile success', 'success'));
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status },
+        });
+
+        dispatch(setAlert('Get profile fail', 'danger'));
+    }
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+    try {
+        const res = await axios.get(`api/profile/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Get profile success', 'success'));
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status },
+        });
+
+        dispatch(setAlert('Get profile fail', 'danger'));
+    }
+};
+
+// Get github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get(`api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data,
         });
 
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import {
+    GET_PROFILE,
+    PROFILE_ERROR,
+    CLEAR_PROFILE,
+    UPDATE_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
+} from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -19,6 +26,18 @@ export default function profile(state = initialState, action) {
                 profile: payload,
                 loading: false,
             };
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false,
+            };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
+                loading: false,
+            };
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -31,6 +50,7 @@ export default function profile(state = initialState, action) {
                 repos: [],
                 loading: false,
             };
+
         default:
             return state;
     }
