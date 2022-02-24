3fc3cb677afbf6f8a30b4da732748ea747f60e3a
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2022-02-24 21:45:09 +0700

    Post reducer, action and initial component

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,3 +17,6 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 
 export const GET_REPOS = 'GET_REPOS';
+
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
