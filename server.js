d0bb7e1d10c5349b2b1d92faa260327c4811c186
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2022-03-14 20:52:14 +0700

    Product Preparation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 // Init middleware
@@ -16,6 +17,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = 5000;
+// Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
